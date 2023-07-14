0d501404537543b8825316c46c1eced71f13e06b
yjcdon <1083137397@qq.com>
2023-07-14 15:25:08 +0800

    666

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//jaiosdj
+
         ImageView visibilityOff = findViewById(R.id.visibilityoff);
         ImageView visibility = findViewById(R.id.visibility);
         EditText password = findViewById(R.id.password);
