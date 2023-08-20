aa46b761108f7f6479b09dd9dda360ca46783ffe
yjcdon <1083137397@qq.com>
2023-08-20 17:50:44 +0800

    要怎么解决切换fragment,照片消失的问题

--- app/src/main/java/com/example/login/ButtonNavigation/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/login/ButtonNavigation/ui/dashboard/DashboardFragment.java
@@ -1,8 +1,14 @@
 package com.example.login.ButtonNavigation.ui.dashboard;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
@@ -25,6 +31,10 @@ import androidx.fragment.app.Fragment;
 
 import com.example.login.R;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class DashboardFragment extends Fragment {
     private ImageView imageViewUpload, imageViewDelete;
     private Button btnUploadImage, btnPublish, btnCamera, btnAlbum, btnCancel;
@@ -33,12 +43,25 @@ public class DashboardFragment extends Fragment {
     private FrameLayout mask;
     private LinearLayout myLinearLayout;
     private ConstraintLayout db_myConstrainLayout;
+    private byte[] imageData;
+
 
     private static final int REQUEST_IMAGES_VIDEO_PERMISSION = 1, REQUEST_CAMERA_PERMISSION = 3, RESULT_CODE_SUCCESS = 100;
     private static final int REQUEST_CODE_ALBUM = 0, REQUEST_CODE_CAMERA = 2;
+    private boolean isUploadSuccess = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
+        if (imageData != null) {
+            Bitmap photo = BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
+            imageViewUpload.setImageBitmap(photo);
+            imageViewDelete.setVisibility(View.VISIBLE);
+            imageViewUpload.setVisibility(View.VISIBLE);
+        }else{
+            Toast.makeText(requireContext(), "照片无了", Toast.LENGTH_SHORT).show();
+        }
 
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
@@ -47,6 +70,8 @@ public class DashboardFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initView(view);
+
+
 
         btnUploadImage.setOnClickListener(view1 -> {
             myLinearLayout.startAnimation(slideInAnimation);
@@ -72,6 +97,12 @@ public class DashboardFragment extends Fragment {
             myLinearLayout.setVisibility(View.GONE);
             mask.setVisibility(View.GONE);
         });
+
+        imageViewDelete.setOnClickListener(view12 -> {
+            imageViewUpload.setVisibility(View.GONE);
+            imageViewDelete.setVisibility(View.GONE);
+        });
+
     }
 
     private void initView(View view) {
@@ -124,4 +155,68 @@ public class DashboardFragment extends Fragment {
             }
         }
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK && data != null) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                Bitmap photo = (Bitmap) extras.get("data");
+                imageViewUpload.setImageBitmap(photo);
+                isUploadSuccess = true;
+
+                // 将Bitmap转换为字节数组
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                photo.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                imageData = stream.toByteArray();
+                mask.setVisibility(View.GONE);
+                // TODO: 调用insertImage函数插入图片，可以使用 getActivity() 获取包含该方法的Activity实例
+
+            } else {
+                isUploadSuccess = false;
+            }
+
+        } else if (requestCode == REQUEST_CODE_ALBUM && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            imageViewUpload.setImageURI(imageUri);
+            isUploadSuccess = true;
+
+            try {
+                // 从Uri中获取图片数据
+                InputStream inputStream = requireActivity().getContentResolver().openInputStream(imageUri);
+                imageData = getBytesFromInputStream(inputStream);
+                mask.setVisibility(View.GONE);
+                // TODO: 执行进一步的操作，如上传图片或其他处理
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                isUploadSuccess = false;
+            }
+        } else {
+            isUploadSuccess = false;
+        }
+
+        if (isUploadSuccess) {
+            requireActivity().setResult(RESULT_CODE_SUCCESS, data);
+            imageViewDelete.setVisibility(View.VISIBLE);
+            myLinearLayout.setVisibility(View.INVISIBLE);
+            imageViewUpload.setVisibility(View.VISIBLE);
+        } else {
+            requireActivity().setResult(RESULT_CANCELED);
+        }
+    }
+
+    private byte[] getBytesFromInputStream(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        byte[] data = new byte[4096];
+        int n;
+        while ((n = inputStream.read(data)) != -1) {
+            buffer.write(data, 0, n);
+        }
+        buffer.flush();
+        return buffer.toByteArray();
+    }
+
 }
